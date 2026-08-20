6299d3261c5b40ca5d5ceca78bd82d687ae3f12f
Vladimir Vasilenko <hattivatt@disroot.org>
2026-08-20 20:28:24 +0700

    syncthing temp off

--- modules/services/syncthing/syncthing.nix
+++ modules/services/syncthing/syncthing.nix
@@ -12,25 +12,25 @@
       key = "${config.sops.secrets.syncthing_key.path}";
       overrideDevices = false;
       settings = {
-        devices = {
-          hatinote = {
-            id = "KHGQWZQ-RBICUDG-5WZCFLC-2WOO5UY-BJ2GZHR-AJBVAYX-LNN4BYS-GXKHCQ7";
-          };
-          "Pixel 9" = {
-            id = "XJLZHCK-SDSGKC3-MLCH6EU-JBUZICN-X4GDWGP-KDVCFCD-VH7EV2Z-OJ4DXQT";
-          };
-        };
-        folders = {
-          "${config.home.homeDirectory}/Notes" = {
-            id = "b76m5-llmfh";
-            label = "Notes";
-            versioning.type = "simple";
-            devices = [
-              "hatinote"
-              "Pixel 9"
-            ];
-          };
-        };
+        # devices = {
+        #   hatinote = {
+        #     id = "KHGQWZQ-RBICUDG-5WZCFLC-2WOO5UY-BJ2GZHR-AJBVAYX-LNN4BYS-GXKHCQ7";
+        #   };
+        #   "Pixel 9" = {
+        #     id = "XJLZHCK-SDSGKC3-MLCH6EU-JBUZICN-X4GDWGP-KDVCFCD-VH7EV2Z-OJ4DXQT";
+        #   };
+        # };
+        # folders = {
+        #   "${config.home.homeDirectory}/Notes" = {
+        #     id = "b76m5-llmfh";
+        #     label = "Notes";
+        #     versioning.type = "simple";
+        #     devices = [
+        #       "hatinote"
+        #       "Pixel 9"
+        #     ];
+        #   };
+        # };
       };
     };
   };
--- modules/system/settings/bluetooth/bluetooth.nix
+++ modules/system/settings/bluetooth/bluetooth.nix
@@ -1,8 +1,19 @@
-{ ... }:
 {
 
-  flake.modules.nixos.bluetooth = {
-    hardware.bluetooth.enable = true;
-    hardware.bluetooth.powerOnBoot = true;
+  flake.modules.nixos.bluetooth =
+    { pkgs, ... }:
+    {
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Experimental = true;
+        };
+      };
+    };
+    environment.systemPackages = [
+      pkgs.overskride
+    ];
   };
 }
